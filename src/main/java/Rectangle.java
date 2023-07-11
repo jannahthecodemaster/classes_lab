16be22abb4303499827f37c2be914ac6a6193aa1
Jannah Anwar <jannah.anwar@outlok.com>
2023-07-11 17:17:14 +0100

    Rectangle task completed

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,27 @@
+public class Rectangle {
+    private int length; //cannot access outside of this class
+    private int width;
+    private int area;
+    private boolean isSquare;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+        this.isSquare = false;
+    }
+     public int getArea() {
+         area = this.length * this.width;
+         return area;
+        }
+
+        public boolean isSquare(){
+        if (length == width) { //highlight text and shift + 9 will put it all into brackets, or quotation marks etc.
+            isSquare = true;
+        }
+        return isSquare;
+        }
+
+ }
+
+
+
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,15 +11,18 @@ public class Runner {
         System.out.println("Running rectangle tasks...");
 
         // 1. Create a new rectangle object
-//        Rectangle rectangle = <INSTANTIATE A NEW RECTANGLE OBJECT HERE>
+        Rectangle rectangle1 = new Rectangle(4, 5);
+
+
 
         // 2. Calculate the rectangle's area and store it in a variable
-//        int area = <CALL YOUR METHOD HERE>;
-//        System.out.println("The rectangle has an area of " + area);
+        int area = rectangle1.getArea();
+        System.out.println("The rectangle has an area of " + area);
 
         // 3. Check if your rectangle is actually a square
-//        boolean isRectangle = <CALL YOUR METHOD HERE>
-//        System.out.println("Object is a square (true/false): " + isRectangle);
+        boolean isRectangle = rectangle1.isSquare();
+           System.out.println("Object is a square (true/false): " + isRectangle);
+
     }
 
     public static void computerDemo(){
